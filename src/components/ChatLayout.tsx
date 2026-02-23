c8f2483216d45173a0d4ccc4f9a6f6709ab32f25
CollisionAcademy <CollisionAcademy@outlook.com>
2026-02-23 04:47:17 -0500

    fix: Remove overflow-hidden class from ChatbotLayout component

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -4,7 +4,7 @@ export default function ChatbotLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-black text-white relative overflow-hidden">
+    <div className="min-h-screen bg-black text-white relative">
       <div className="mx-auto max-w-[1600px] px-6 py-8">
         {children}
       </div>
