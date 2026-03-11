269dc08af7c705e69860a26679123e0ac2c44b7a
CollisionAcademy <CollisionAcademy@outlook.com>
2026-03-11 01:11:53 -0400

    fix egnyte oauth endpoint

--- src/app/api/egnyte/login/route.ts
+++ src/app/api/egnyte/login/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
   const redirectUri =
     "https://collision-academy-new-git-cha-bfa414-collision-academy-82dbb1d7.vercel.app/oauth/egnyte/callback"
 
-  const url = `https://collisionacademy.egnyte.com/puboauth/authorize?response_type=code&client_id=${process.env.EGNYTE_CLIENT_ID}&redirect_uri=${encodeURIComponent(
+  const url = `https://oauth.egnyte.com/oauth2/authorize?response_type=code&client_id=${process.env.EGNYTE_CLIENT_ID}&redirect_uri=${encodeURIComponent(
     redirectUri
   )}`
 
